3b1cc25c7d8e6dc49fe2300b66ccbb4fa3583e2b
Einar Lielmanis <einars@gmail.com>
2010-09-21 15:13:01 +0300

    XMonad stuff

---
+++ dots/xmonad.hs
@@ -0,0 +1,182 @@
+import XMonad
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import System.Exit
+import Graphics.X11.Xlib
+import System.IO
+import Data.List
+
+import XMonad.Actions.GridSelect
+
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.WindowProperties
+
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+
+import XMonad.Prompt.Shell
+import XMonad.Prompt
+
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Reflect
+import XMonad.Layout.IM
+import XMonad.Layout.Tabbed
+
+
+main = do
+        xmproc <- spawnPipe "xmobar"
+        xmonad
+ --             $ withUrgencyHookC dzenUrgencyHook
+ --             { args = ["-bg", "darkblue", "-xs", "1"]}
+ --             UrgencyConfig { suppressWhen = Focused, remindWhen = Every 5 }
+
+                $ defaultConfig
+                { modMask            = mod4Mask
+                , manageHook         = customManageHook
+                , layoutHook         = customLayoutHook
+                , borderWidth        = 1
+                , normalBorderColor  = "#444444"
+                , focusedBorderColor = "#ff3333"
+                , workspaces         = ["1:web", "2:code", "3:chat", "4:manual", "5:fullscreen", "6:gimp", "7", "8", "9"]
+                , focusFollowsMouse  = True
+                , terminal           = "urxvt"
+                , logHook            = customLogHook xmproc
+                , keys               = customKeys
+                }
+
+
+
+customLogHook h = dynamicLogWithPP $ customPP { ppOutput = hPutStrLn h }
+
+
+customPP = defaultPP
+    { ppHidden  = xmobarColor "#999" "" . wrap "" " "
+    , ppVisible = xmobarColor "#999" "" . wrap "" "·"
+    , ppCurrent = xmobarColor "#6c6" "" . wrap "" " "
+    , ppUrgent  = xmobarColor "#F00" "" . wrap "" "!"
+    , ppLayout  = xmobarColor "#333" ""
+    , ppTitle   = xmobarColor "#ccc" "" . shorten 200
+    , ppSep = "<fc=#444> | </fc>"
+    }
+
+-- tab bar
+customXPConfig = defaultXPConfig
+    { font     = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u"
+    , fgColor  = "#ddddff"
+    , bgColor  = "#000000"
+    , bgHLight = "#000000"
+    , fgHLight = "#FF0000"
+    , position = Top
+    }
+
+customManageHook = (composeAll . concat $
+    [ [ isFullscreen                    --> doFullFloat
+    , className =? "OpenOffice.org 3.1" --> doShift "4:manual"
+    , className =? "Evince"             --> doShift "4:manual"
+    , className =? "Okular"             --> doShift "4:manual"
+    , className =? "Xmessage"           --> doCenterFloat
+    , className =? "Gimp"               --> doShift "6:gimp"
+    , className =? "Pidgin"             --> doShift "3:chat"
+    , className =? "Skype"              --> doShift "3:chat"
+ -- , className =? "Skype"              --> doFloat
+--, title     =? "einars.lielmanis - Skype™ (Beta)" --> doCenterFloat
+    , className =? "MPlayer"            --> doShift "5:fullscreen"
+    , className =? "Smplayer"           --> doShift "5:fullscreen"
+    ]
+    , [title    =? t                    --> doFloat | t <- titleFloats ]
+    , [title    =? t                    --> doCenterFloat | t <- centerFloats ]]
+    ) <+> manageDocks
+    where titleFloats = [ "Downloads", "Send file", "Open", "File Transfers", "XChat: Network List", "Save screenshot as..."]
+          centerFloats = [ "Caml graphics" ]
+
+
+customLayoutHook
+    = onWorkspace "3:chat" imLayout
+    $ onWorkspace "1:web" webL
+    $ onWorkspace "6:gimp" gimpL
+    $ onWorkspace "5:fullscreen" fullL
+    $ standardLayouts
+   where
+        standardLayouts = avoidStruts  $ (tabLayout ||| tiled |||  reflectTiled ||| Mirror tiled ||| Full)
+        tiled           = smartBorders (ResizableTall 1 (2/100) (1/2) [])
+        reflectTiled    = (reflectHoriz tiled)
+        tabLayout       = (tabbed shrinkText defaultTheme)
+        full            = noBorders Full
+
+        --imLayout        = tiled
+        imLayout        = avoidStruts $ withIM (0.22) isSkype (Mirror tiled ||| Full)
+                        where isSkype = (Title "einars.lielmanis - Skype™ (Beta)")
+
+        gimpL = avoidStruts $ smartBorders $ withIM (0.11) (Role "gimp-toolbox") $ reflectHoriz $ withIM (0.15) (Role "gimp-dock") Full
+        webL  = avoidStruts $ tabLayout  ||| tiled ||| reflectHoriz tiled |||  full
+        fullL = avoidStruts $ full
+
+
+customKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask,               xK_Return), spawn $ XMonad.terminal conf)
+    , ((modMask .|. shiftMask, xK_c ),     kill)
+
+    , ((modMask,               xK_p),      shellPrompt customXPConfig)
+    , ((modMask,               xK_r),      shellPrompt customXPConfig)
+    , ((modMask,               xK_g),      goToSelected defaultGSConfig)
+
+    , ((modMask,               xK_space ), sendMessage NextLayout)
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modMask .|. shiftMask, xK_b ),     sendMessage ToggleStruts)
+
+    -- floating layer stuff
+    , ((modMask,               xK_t ),     withFocused $ windows . W.sink)
+
+    , ((modMask,               xK_n ),     refresh)
+
+    , ((modMask,               xK_Tab ),   windows W.focusDown)
+    , ((modMask .|. shiftMask, xK_Tab ),   windows W.focusUp)
+    , ((modMask,               xK_j ),     windows W.focusDown)
+    , ((modMask,               xK_k ),     windows W.focusUp)
+ -- , ((modMask,               xK_m ),     windows W.focusMaster)
+
+ -- , ((modMask .|. shiftMask, xK_Return), windows W.swapMaster)
+    , ((modMask .|. shiftMask, xK_Return), spawn "gvim")
+    , ((modMask .|. shiftMask, xK_j ),     windows W.swapDown )
+    , ((modMask .|. shiftMask, xK_k ),     windows W.swapUp )
+
+    , ((modMask,               xK_comma),  sendMessage (IncMasterN 1))
+    , ((modMask,               xK_period), sendMessage (IncMasterN (-1)))
+
+    , ((modMask,               xK_h ),     sendMessage Shrink)
+    , ((modMask,               xK_l ),     sendMessage Expand)
+    , ((modMask .|. shiftMask, xK_h ),     sendMessage MirrorShrink)
+    , ((modMask .|. shiftMask, xK_l ),     sendMessage MirrorExpand)
+
+    , ((modMask .|. shiftMask, xK_p ),     io (exitWith ExitSuccess))
+    , ((modMask,               xK_p ),     restart "xmonad" True)
+
+    , ((modMask,               xK_m     ), spawn "run-inferior super-m")
+    , ((modMask .|. shiftMask, xK_m     ), spawn "run-inferior super-M")
+    , ((modMask,               xK_t     ), spawn "thunar torrents")
+    , ((modMask,               xK_q     ), spawn "thunar stuff")
+    , ((modMask,               xK_b     ), spawn "thunar /storage/books")
+    , ((modMask,               xK_d     ), spawn "thunar Documents")
+
+    -- xdotool script to help refresh opera after vim save, :w
+    , ((modMask,               xK_w     ), spawn "xmonad-refresh-opera")
+
+    ]
+    ++
+    -- mod-[1..9] %! Switch to workspace N
+    -- mod-shift-[1..9] %! Move client to workspace N
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    ++
+
+    -- mod-[a,s] %! switch to twinview screen 1/2
+    -- mod-shift-[a,s] %! move window to screen 1/2
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_s, xK_a] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
